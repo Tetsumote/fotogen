d933095cf85900b67dbfcf66e1549938b65b86f2
Karl Kroening <karlk@kralnet.us>
2017-06-07 18:50:05 -0600

    Initial commit

---
+++ dl.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+curl -O http://fotogenerator.npocloud.nl/index.html
+curl -O http://fotogenerator.npocloud.nl/dekennisvannu-input.png
+curl -O http://fotogenerator.npocloud.nl/dekennisvannu-output.png
+curl -O http://fotogenerator.npocloud.nl/editor.png
+curl -O http://fotogenerator.npocloud.nl/dekennisvannu-sheet.jpg 
